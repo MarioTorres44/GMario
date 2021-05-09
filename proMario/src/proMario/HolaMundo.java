693e3a9917e129854e71ec6eaccee466c4352a1d
RTC <RTC@I121544.once.intranet.local>
2021-05-09 17:24:37 +0200

    Segundo cambio

--- proMario/src/proMario/HolaMundo.java
+++ proMario/src/proMario/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HolaMundo1");
+		System.out.println("Hola de nuevo1");
 	}
 
 }
