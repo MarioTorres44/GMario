c5ae43015ed887ed15ff1e1cc10ac40e78291ef5
RTC <RTC@I121544.once.intranet.local>
2021-05-09 17:43:19 +0200

    3 CambioSI

--- proMario/src/proMario/HolaMundo.java
+++ proMario/src/proMario/HolaMundo.java
@@ -7,6 +7,7 @@ public class HolaMundo {
 		System.out.println("HolaMundo1");
 		System.out.println("Hola de nuevo1");
 		System.out.println("HolaMundo 3 Cambio");
+		System.out.println("3 Cambio");
 	}
 
 }
